aa038199b4ea62a96438d6c3caca49815cd8f0d6
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-22 01:05:46 -0500

    create dealer_hand

--- BlackJack.py
+++ BlackJack.py
@@ -225,3 +225,7 @@ while True:
     player_hand = Hand()
     player_hand.add_card(deck.deal())
     player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
